c8b7c516b48b604ab88d39e3cb869feefec7c453
locnv <locnv14@gmail.com>
2025-07-21 17:18:30 +0700

    first commit

---
+++ src/data/role-titles.ts
@@ -0,0 +1,13 @@
+export const ROLE_TITLES = [
+  "Front-End Developer",
+  "UI/UX Enthusiast",
+  "JavaScript Developer",
+  "React.js Developer",
+  "Next.js Developer",
+  "UI/UX Designer",
+  "Graphic Designer",
+  "Animation Developer",
+  "Motion Developer",
+  "Smooth Transition",
+  "WebGL Developer",
+]
